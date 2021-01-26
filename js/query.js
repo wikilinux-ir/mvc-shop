ec9d7798fd21f1ecb7f5c257cf1f5a90e83c3999
mr-noip <admin@wikilinux.ir>
2021-01-27 02:48:56 +0330

    end of slider2

--- js/query.js
+++ js/query.js
@@ -40,7 +40,7 @@ $('.item_menu2').hover(function(){
 //  timer[timerAttr] = timerAttr;
     menut[menu3Attr] = setTimeout(function() {
         tag.addClass('active_submenu');
-        $(' > .menut',tag).slideDown();
+        $(' > .menut',tag).fadeIn(100);
     },300); 
 
 },function(){
@@ -51,7 +51,7 @@ $('.item_menu2').hover(function(){
 //  timer[timerAttr] = timerAttr;
     menut[menu3Attr] = setTimeout(function() {
         tag.removeClass('active_submenu');
-        $(' > .menut',tag).slideUp();
+        $(' > .menut',tag).fadeOut();
         
     },400); 
 });
@@ -63,29 +63,122 @@ function newFunction(timerAttr) {
 
 
 var sliderTag = $('.slider_content');
-var items= sliderTag.find('.slider_items');
-var sliderItems= 1;
-function slider(){
-    items.eq(sliderItems - 1).hide();
-    items.eq(sliderItems - 1).fadeIn(100);
+var items = sliderTag.find('.slider_items');
+var navItems= $('.navigator_items').find('li');
+var countItems = items.length;
+var sliderItems= 0;
+function slider_show(){
+    // alert(items.eq(1));
+    // console.log(countItems);
+    //items.hide();
+    items.fadeOut(300);
+    items.eq(sliderItems).fadeIn(600);
+    
+    navItems.removeClass('active_slider');
+    navItems.eq(sliderItems).addClass('active_slider');
+
     sliderItems++;
-    alert('aa');
+
+    if (sliderItems > (countItems - 1)) {
+        sliderItems=0;
+    }
+    
 }
-$('.btn').hover(slider());
-// $(document).ready(function(){
-    
-//     while(sliderItems <= 5){
-    
-//         slider();
-//         sleep(2000);
-//     }
-    
+
+var sliderTimer = setInterval(function(){
+    slider_show();
+},5000);
+
+$(navItems).hover(function(){
+    clearInterval(sliderTimer);
+},function(){
+    sliderTimer = setInterval(function(){
+        slider_show();},5000);
+});
+
+// $('.btn').click(function(){
+//     slider_show();
 // });
 
-// function sleep(milliseconds) {
-//     const date = Date.now();
-//     let currentDate = null;
-//     do {
-//       currentDate = Date.now();
-//     } while (currentDate - date < milliseconds);
-//   }
+function nextslide(){
+    slider_show();
+}
+
+$('.nextslide').click(function(){
+    nextslide();
+});
+
+
+function prevslide(){
+    if (sliderItems < 0){
+        sliderItems=items.length - 1;//reset slider to end of slider
+    }
+    sliderItems = sliderItems - 2 ;
+    slider_show();
+}
+$('.prevslide').click(function(){
+    prevslide();
+});
+
+$(document).ready(function(){
+    slider_show();
+});
+
+function goToSlide(index){
+    sliderItems=index;
+    slider_show();
+}
+
+$('.navigator_items li').hover(function(){
+    var index = $(this).index();
+    goToSlide(index);
+},function(){});
+
+
+
+
+var sliderItemTwo = $('.logo_slider2').find('.item_slider2');
+var sliderNavTwo = $('.right_slider_nav').find('li');
+var oldPrice = $('.old_price').find('span');
+var newPrice = $('.new_price').find('span');
+var shortInfo = $('.short_info').find('div');
+var sliderNumTwo = 0;
+
+function slider2() {
+
+    sliderItemTwo.hide(200);
+    oldPrice.hide(200);
+    newPrice.hide(200);
+    shortInfo.hide(200);
+    sliderItemTwo.eq(sliderNumTwo).fadeIn();
+    oldPrice.eq(sliderNumTwo).fadeIn();
+    newPrice.eq(sliderNumTwo).fadeIn();
+    shortInfo.eq(sliderNumTwo).fadeIn();
+    sliderNavTwo.removeClass('active_slider2');
+    sliderNavTwo.eq(sliderNumTwo).addClass('active_slider2');
+    sliderNumTwo++;
+    if (sliderNumTwo > sliderNavTwo.length - 1) {
+        sliderNumTwo=0;
+    }
+    if (sliderNumTwo < 0) {
+        sliderNumTwo = sliderNavTwo.length - 1;       
+    }
+}
+
+slider2();
+var sliderTowTimer=setInterval(function(){
+    slider2();
+},5000);
+
+function gotoSliderTwoslide(sTi){
+    sliderNumTwo = sTi;
+    slider2();
+}
+
+$('.right_slider_nav li').click(function(){
+    var sTi= $(this).index();
+    gotoSliderTwoslide(sTi);
+});
+
+
+$('.flipTimer').flipTimer({ direction: 'down', date: 'Jan 27, 2021 03:34:00'});
